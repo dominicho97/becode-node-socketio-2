aa81d71843174634f504334c94c0ec81a8efe07c
dominicho97 <dominic.ho97@gmail.com>
2019-02-26 10:15:20 +0100

    made connection with socket

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const socket = require('socket.io');
 //App setup
 const app = express();
 const server = app.listen(4000, function () {
@@ -8,3 +8,10 @@ const server = app.listen(4000, function () {
 
 //Static files
 app.use(express.static('public'))
+
+
+//Socket setup 
+const io = socket(server);
+io.on('connection', function (socket) {
+  console.log('made socket connection', socket.id)
+})
